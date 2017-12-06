a5688f1c69ecc916dda558c6937801152c53d5d2
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-12-06 15:24:32 +0100

    latest changes

--- apps/cmsocial/src/cmsocial_login.erl
+++ apps/cmsocial/src/cmsocial_login.erl
@@ -7,6 +7,14 @@ spec() ->
 
 key() -> login.
 
+do(#{<<"username">> := <<"a@b">>,
+     <<"password">> := <<"a">>}, _) ->
+     P = #{ id => <<"testuser">>,
+            first => <<"Test">>,
+            last => <<"Last">>,
+            username => <<"a@b">> },
+     {ok, login, P, P};
+
 do(#{<<"username">> := U,
      <<"password">> := P}, S) when map_size(S) == 0 ->
     case cmdb:s(names, U, user) of
