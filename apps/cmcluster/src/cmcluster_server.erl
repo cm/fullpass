ccd5af54ed5e197a24b2683571ecc282ba895230
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-12-25 19:15:57 +0100

    latest changes

--- apps/cmcluster/src/cmcluster_server.erl
+++ apps/cmcluster/src/cmcluster_server.erl
@@ -13,16 +13,13 @@ start_link() ->
     gen_statem:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 all_nodes() -> 
-    Members = pg2:get_members(cmcluster),
-    {ok, [ gen_statem:call(M, nodes) || M <- Members ]}.
+    {ok, [ gen_statem:call({cmcluster_server, N}, nodes) || N <- net_adm:world() ]}.
 
 init([]) ->
-    pg2:create(cmcluster),
     net_kernel:monitor_nodes(true),
     net_adm:world(),
     case length(expected_nodes()) of
         1 -> 
-            join(green),
             cmkit:log({cmcluster, green, standalone}),
             {ok, green, #data{}};
         _ -> 
@@ -66,8 +63,7 @@ red({call, From}, {delete_table_replica, Node, Tab}, Data) ->
 
 yellow(info, {nodeup, Node}, Data) ->
     State = state(),
-    {ok, JoinStatus} = join(State),
-    cmkit:log({cmcluster, State, nodeup, Node, JoinStatus}),
+    cmkit:log({cmcluster, State, nodeup, Node}),
     {next_state, State, Data};
 
 yellow(info, {nodedown, Node}, Data) ->
@@ -101,8 +97,7 @@ yellow({call, From}, {delete_table_replica, Node, Tab}, Data) ->
 
 green(info, {nodeup, Node}, Data) ->
     State = state(),
-    {ok, JoinStatus} = join(State),
-    cmkit:log({cmcluster, State, nodeup, Node, JoinStatus}),
+    cmkit:log({cmcluster, State, nodeup, Node}),
     {next_state, State, Data};
 
 green(info, {nodedown, Node}, Data) ->
@@ -133,19 +128,6 @@ green({call, From}, {create_table_replica, Node, Tab, Media}, Data) ->
 green({call, From}, {delete_table_replica, Node, Tab}, Data) ->
     Res = delete_table_replica(Node, Tab),
     {keep_state, Data, {reply, From, Res}}.
-
-join(green) ->
-    LocalMembers = pg2:get_local_members(cmcluster),
-    case lists:member(self(), LocalMembers) of
-        true -> 
-            {ok, already_in_cluster};
-        false ->
-            pg2:join(cmcluster, self()),
-            {ok, joined_cluster}
-    end;
-
-join(_) -> 
-    {ok, waiting_to_join_cluster}.
 
 expected_nodes() ->
     Sname = erlang:binary_to_list(cmkit:sname()),
